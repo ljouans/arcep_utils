679e939fa29d19b4fbf51213c255a11d6dcfea97
Loïc Jouans <loic.jouans@arcep.fr>
2021-07-05 14:55:34 +0200

    Commentaires

--- pathtools.py
+++ pathtools.py
@@ -6,54 +6,80 @@ from typing import Union
 Outils de manipulation et de gestion de chemin.
 """
 
+
 def _get_tool_path() -> Path:
-	return Path(os.path.realpath(__file__)).parent
+    return Path(os.path.realpath(__file__)).parent
+
 
 def data_path() -> Path:
-	"""
-	En notant parent/utils/pathtools.py l'arborescence du fichier actuel, renvoi 
-	le chemin absolu vers le dossier parent/data/.
-	Crée le dossier si nécessaire
-	"""
-	path = (_get_tool_path() /  '../data').resolve()
-	path.mkdir(parents=True, exist_ok=True)
-	return path
-	
+    """
+    En notant parent/utils/pathtools.py l'arborescence du fichier actuel, renvoi
+    le chemin absolu vers le dossier parent/data/.
+    Crée le dossier si nécessaire
+    """
+    path = (_get_tool_path() / "../data").resolve()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
 def prod_path() -> Path:
-	"""
-	Renvoi le chemin absolu vers le dossier {data}/prod/
-	"""
-	path = _get_tool_path().parent / 'data/prod/'
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """
+    Renvoi le chemin absolu vers le dossier {data}/prod/
+    """
+    path = _get_tool_path().parent / "data/prod/"
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
 
 def tmp_path() -> Path:
-	path = (_get_tool_path() / '../data/tmp').resolve()
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """Chemin vers le dossier {ce fichier}/../data/tmp. Crée le dossier si nécessaire.
+
+    Returns:
+        Path: le chemin
+    """
+    path = (_get_tool_path() / "../data/tmp").resolve()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
 
 def config_path() -> Path:
-	path = (_get_tool_path() / '../config').resolve()
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """Chemin vers le dossier {ce fichier}/../config. Crée le dossier si nécessaire.
 
+    Returns:
+        Path: le chemin
+    """
+    path = (_get_tool_path() / "../config").resolve()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
 def outer_out_path() -> Path:
-	path = (_get_tool_path() / '../../out').resolve()
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """Chemin vers le dossier {ce fichier}/../../out/. Crée le dossier si nécessaire.
+
+    Returns:
+        Path: le chemin
+    """
+    path = (_get_tool_path() / "../../out").resolve()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
 
 def outer_test_path() -> Path:
-	path = (_get_tool_path() / '../../tests').resolve()
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """Chemin vers le dossier {ce fichier}/../../test/. Crée le dossier si nécessaire.
+
+    Returns:
+        Path: le chemin
+    """
+    path = (_get_tool_path() / "../../tests").resolve()
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
 
 def specific_datapath(stem: Union[str, Path]):
-	"""
-	Renvoi le chemin absolu vers le dossier {data}/stem/
-	Créé les dossiers nécessaires en chemin.
-	"""
-	path = data_path() / stem
-	path.mkdir(parents=True, exist_ok=True)
-	return path
+    """
+    Renvoi le chemin absolu vers le dossier {data}/stem/
+    Créé les dossiers nécessaires en chemin.
+    """
+    path = data_path() / stem
+    path.mkdir(parents=True, exist_ok=True)
+    return path
