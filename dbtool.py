d1439405183b867a2ccebcf5a2c92eb8d0622466
Loïc Jouans <loic.jouans@arcep.fr>
2021-07-05 14:43:07 +0200

    Commentaire, formattage et simplification de dbtool

--- dbtool.py
+++ dbtool.py
@@ -1,112 +1,171 @@
-## Tools & helpers
-import logging
 import hashlib
+import logging
 import os
+import warnings
 from typing import Any, Callable, Dict, Optional, Sequence, Union
-import fiona
-import geopandas as pdg
-from geopandas.geodataframe import GeoDataFrame  # type: ignore
-import pandas as pd
-import warnings; warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
+
 from configparser import ConfigParser
-from sqlalchemy import create_engine
-import sqlalchemy as sqa
-from . import pathtools as pth
 from pathlib import Path
 
+import sqlalchemy as sqa
+from sqlalchemy import create_engine
+import geopandas as pdg
+import pandas as pd
+
+from . import pathtools as pth
+from .argstruct.geo_table_info import GeoInfo
+
+warnings.filterwarnings("ignore", message=".*initial implementation of Parquet.*")
+
+def _create_dir(folder_path: Path):
+    folder_path.mkdir(exist_ok=True)
+
+
 class Tool:
-	def __init__(self, tmpDir: Optional[Path] = None, userFileName: str='', secretPathFile: Optional[Union[Path, str]] = None, connection_string: Optional[str] = None):
-		if tmpDir is None:
-			tmpDir = pth.tmp_path()
+    """
+    Outil de connexion et de requête en base, de chargement de geo/dataframe.
+    """
+    def __init__(
+        self,
+        tmpDir: Optional[Path] = None,
+        secretPathFile: Optional[Union[Path, str]] = None,
+        connection_string: Optional[str] = None,
+    ):
+        if tmpDir is None:
+            tmpDir = pth.tmp_path()
 
-		self._tmp = tmpDir
-		self._userFileName = userFileName
-		self._thisRandom = str(hash(userFileName))
-		self._connexion_string = ''
-		self._engine = self._create_engine(secretPathFile, connection_string)
-		
-		
-	@property
-	def connexion_string(self):
-		return self._connexion_string
+        self._tmp = tmpDir
+        self._connexion_string = ""
+        self._engine = self._create_engine(secretPathFile, connection_string)
 
-	@property
-	def tmp(self):
-		return self._tmp / self._thisRandom
-	
-	
-	def _create_dir(self, folderPath:Path):
-		folderPath.mkdir(exist_ok=True)
+    @property
+    def tmp(self) -> Path:
+        """Chemin du dossier des temporaires utilisé pat dbtools.py.
+        Relatifs uniquement au fichier pathtools.py
 
-	def _create_engine(self, secretPathFile: Optional[Union[str, Path]] = None, connectionString: Optional[str] = None):
-		if connectionString is None:
-			parser = ConfigParser()
+        Returns:
+            Path: chemin vers le dossier des tmp
+        """
+        return self._tmp
 
-			secretpath = pth._get_tool_path() / 'secret/db.cfg'
-			secretpath = Path(secretPathFile) if secretPathFile is not None else secretpath
+    @property
+    def connexion_string(self) -> str:
+        """Retourne la chaine de connexion utilisée pour parler avec la base
 
-			if not secretpath.exists():
-				raise FileNotFoundError('Could not find the secret folder. Please specify the connexion secrets')
-
-			_ = parser.read(secretpath)
-			connectionString = parser.get('Collecte03_dev', 'conn_string')
-
-		self._connexion_string = connectionString
-		engine = create_engine(connectionString)
-		return engine
-
-	def has_table(self, table_name: str, schema: str) -> bool:
-		insp = sqa.inspect(self._engine)
-		return insp.has_table(table_name, schema=schema)
-		
-	def _get_crs(self, table_name: str, geo_col: str, schema: str) -> str:
-		df = pd.read_sql(f'SELECT ST_SRID({geo_col}) FROM {schema}.{table_name} where {geo_col} is not NULL LIMIT 1;', self._engine)
-		return str(df['st_srid'].values[0])
-		
-		
-	def fetch_query(self, query: str, storeIt: bool=True, geo_col: Optional[str]=None, geo_table_name: Optional[str] = None, geo_schema_name: str = 'test_loic', force_refetch: bool=False, params: Optional[Dict[str, Any]]=None) -> Union[pd.DataFrame, pdg.GeoDataFrame]:
-
-		crs = None
-		if geo_col is not None:
-			if geo_table_name is None:
-				logging.warn('I strongly suggest you specify the name of the table containing the geographic informations so that I can get the right CRS.')
-			else:
-				crs = 'EPSG:' + self._get_crs(geo_table_name, geo_col, schema=geo_schema_name)
-				logging.debug(f'Found CRS = {crs}')
-		   
+        Returns:
+            str: chaine de connexion
+        """
+        return self._connexion_string
 
 
-		if geo_col is None:
-			_loader = pd.read_feather # type: ignore
-		else:
-			_loader = pdg.read_feather # type: ignore
-		_loader: Callable[[str, Optional[Sequence[str]], Optional[bool]], pd.DataFrame]
+    def _create_engine(
+        self,
+        secret_path_file: Optional[Union[str, Path]] = None,
+        connection_string: Optional[str] = None,
+    ):
+        if connection_string is None:
+            parser = ConfigParser()
 
-		self._create_dir(self.tmp)
+            secretpath = pth._get_tool_path() / "secret/db.cfg"
+            secretpath = Path(secret_path_file) if secret_path_file is not None else secretpath
 
-		eqry = str(query) + str(geo_col) + str(crs)
-		savePath = self.tmp / (str(hashlib.md5(eqry.encode('UTF8')).hexdigest()) + '.fthr')
-		loadedFromServer = False
+            if not secretpath.exists():
+                raise FileNotFoundError(
+                    "Could not find the secret folder. Please specify the connexion secrets"
+                )
 
-		# Load
-		if storeIt and os.path.exists(savePath) and not force_refetch:
-			df = _loader(savePath)  # type: ignore
-		else:
-			df = pd.read_sql(query, self._engine, params=params)
-			loadedFromServer = True
+            _ = parser.read(secretpath)
+            connection_string = parser.get("Collecte03_dev", "conn_string")
 
-		# Parse
-		if loadedFromServer and geo_col is not None:
-			gk = pdg.GeoSeries.from_wkb(df[geo_col])  #  type: ignore
-			df = pdg.GeoDataFrame(df, geometry=gk, crs=crs)
-			df = df.drop([geo_col], axis=1)  # type: ignore
+        self._connexion_string = connection_string
+        engine = create_engine(connection_string)
+        return engine
 
+    def has_table(self, table_name: str, schema: str) -> bool:
+        """Teste si <schema>.<table_name> existe dans la base cible
 
-		# Save
-		if storeIt and not df.empty:
-			df.to_feather(str(savePath))
-		
-		if df.empty:
-			logging.warn(f'The dataframe from the following query was empty\n{query}')
+        Args:
+            table_name (str): nom de table
+            schema (str): nom du schema
 
-		return df
+        Returns:
+            bool: True ssi la table existe
+        """
+        insp = sqa.inspect(self._engine)
+        return insp.has_table(table_name, schema=schema)
+
+    def _get_crs(self, table_name: str, geo_col: str, schema: str) -> str:
+        df = pd.read_sql(
+            f"SELECT ST_SRID({geo_col}) FROM {schema}.{table_name} where {geo_col} is not NULL"
+            " LIMIT 1;",
+            self._engine,
+        )
+        return str(df["st_srid"].values[0])
+
+    def fetch_query(
+        self,
+        query: str,
+        store_it: bool = True,
+        geo_info: Optional[GeoInfo] = None,
+        force_refetch: bool = False,
+        params: Optional[Dict[str, Any]] = None,
+    ) -> Union[pd.DataFrame, pdg.GeoDataFrame]:
+        """Exécute une requête qui va chercher des données en base et les formatte en Geo/Dataframe.
+
+        Args:
+            query (str): Requête à exécuter. DOIT RETOURNER DES VALEURS
+            storeIt (bool, optional): Stocker ou non le résultat en cache. Defaults to True.
+            geo_info (Optional[str], optional): informations sur la colonne contenant une géométrie,
+                            si elle existe. Nécessaire pour la charger correctement dans Geopandas.
+            force_refetch (bool, optional): Ignore le cache. Defaults to False.
+            params (Optional[Dict[str, Any]], optional): Paramètres de requêtes supplémentaires.
+                                                         Defaults to None.
+
+        Returns:
+            Union[pd.DataFrame, pdg.GeoDataFrame]: La géo/dataframe contenant les données requêtées.
+        """
+
+        crs = None
+        if geo_info is not None:
+            if geo_info.schema is None:
+                logging.warning(
+                    "I strongly suggest you specify the name of the table containing the geographic"
+                    " informations so that I can get the right CRS."
+                )
+            else:
+                crs = "EPSG:"+self._get_crs(geo_info.table, geo_info.column, schema=geo_info.schema)
+                logging.debug("Found CRS = %s", crs)
+
+        if geo_info is None:
+            _loader = pd.read_feather  # type: ignore
+        else:
+            _loader = pdg.read_feather  # type: ignore
+        _loader: Callable[[str, Optional[Sequence[str]], Optional[bool]], pd.DataFrame]
+
+        _create_dir(self.tmp)
+
+        eqry = str(query) + str(geo_info) + str(crs)
+        save_path = self.tmp / (str(hashlib.md5(eqry.encode("UTF8")).hexdigest()) + ".fthr")
+        loaded_from_server = False
+
+        # Load
+        if store_it and os.path.exists(save_path) and not force_refetch:
+            df = _loader(save_path)  # type: ignore
+        else:
+            df = pd.read_sql(query, self._engine, params=params)
+            loaded_from_server = True
+
+        # Parse
+        if loaded_from_server and geo_info is not None:
+            gk = pdg.GeoSeries.from_wkb(df[geo_info.column])  #  type: ignore
+            df = pdg.GeoDataFrame(df, geometry=gk, crs=crs)
+            df = df.drop([geo_info.column], axis=1)  # type: ignore
+
+        # Save
+        if store_it and not df.empty:
+            df.to_feather(str(save_path))
+
+        if df.empty:
+            logging.warning("The dataframe from the following query was empty\n%s", query)
+
+        return df
