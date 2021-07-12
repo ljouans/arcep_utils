41e00cc93af1515595cf85a7ac7838327692ec3e
Loïc Jouans <loic.jouans@arcep.fr>
2021-07-12 11:27:27 +0200

    Ajout d'un paramètre en ligne de commande pour ne pas sauter les tests marqués comme 'skip'

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,26 @@ import os
 import pathlib
 import shutil
 
+import pytest
+import _pytest.skipping
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--no-skips",
+        action="store_true",
+        default=False, help="disable skip marks")
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_cmdline_preparse(config, args):
+    if "--no-skips" not in args:
+        return
+
+    def no_skip(*args, **kwargs):
+        return
+
+    _pytest.skipping.skip = no_skip
 
 
 def pytest_sessionstart(session):
