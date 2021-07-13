a20b6a10d2757ee1bd156aff2dbc884c2c258734
Loïc Jouans <loic.jouans@arcep.fr>
2021-07-13 12:51:07 +0200

    Donne l'option de spécifier le chemin de base aux outils

--- pathtools.py
+++ pathtools.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional
 
 """
 Outils de manipulation et de gestion de chemin.
@@ -11,51 +11,57 @@ def _get_tool_path() -> Path:
     return Path(os.path.realpath(__file__)).parent
 
 
-def data_path() -> Path:
+def data_path(base_path: Optional[Union[Path, str]] = None) -> Path:
     """
     En notant parent/utils/pathtools.py l'arborescence du fichier actuel, renvoi
     le chemin absolu vers le dossier parent/data/.
     Crée le dossier si nécessaire
     """
-    path = _get_tool_path().parent / "data"
+    path = base_path if base_path is not None else _get_tool_path().parent
+    path /= "data/"
     path.mkdir(parents=True, exist_ok=True)
     return path
 
 
-def prod_path() -> Path:
+def prod_path(base_path: Optional[Union[Path, str]] = None) -> Path:
     """
     Renvoi le chemin absolu vers le dossier {data}/prod/
     """
-    path = _get_tool_path().parent / "data/prod/"
+    path = base_path if base_path is not None else _get_tool_path().parent
+    path /= "data/prod/"
     path.mkdir(parents=True, exist_ok=True)
     return path
 
 
-def tmp_path() -> Path:
+def tmp_path(base_path: Optional[Union[Path, str]] = None) -> Path:
     """Chemin vers le dossier {ce fichier}/../data/tmp. Crée le dossier si nécessaire.
 
     Returns:
         Path: le chemin
     """
-    path = _get_tool_path().parent / "data/tmp"
+    path = base_path if base_path is not None else _get_tool_path().parent
+    path /= "data/tmp"
     path.mkdir(parents=True, exist_ok=True)
     return path
 
 
-def config_path() -> Path:
+def config_path(base_path: Optional[Union[Path, str]] = None) -> Path:
     """Chemin vers le dossier {ce fichier}/../config. Crée le dossier si nécessaire.
 
     Returns:
         Path: le chemin
     """
-    path = _get_tool_path().parent / "config"
+    path = base_path if base_path is not None else _get_tool_path().parent
+    path /= "config"
     path.mkdir(parents=True, exist_ok=True)
     return path
+
 
 def outer_path() -> Path:
     path = _get_tool_path().parent.parent
     path.mkdir(parents=True, exist_ok=True)
     return path
+
 
 def outer_out_path() -> Path:
     """Chemin vers le dossier {ce fichier}/../../out/. Crée le dossier si nécessaire.
@@ -66,8 +72,6 @@ def outer_out_path() -> Path:
     path = _get_tool_path().parent.parent / "out"
     path.mkdir(parents=True, exist_ok=True)
     return path
-
-
 
 
 def outer_test_path() -> Path:
@@ -81,11 +85,11 @@ def outer_test_path() -> Path:
     return path
 
 
-def specific_datapath(stem: Union[str, Path]):
+def specific_datapath(stem: Union[str, Path], base_path: Optional[Union[Path, str]] = None):
     """
     Renvoi le chemin absolu vers le dossier {data}/stem/
     Créé les dossiers nécessaires en chemin.
     """
-    path = data_path() / stem
+    path = data_path(base_path=base_path) / stem
     path.mkdir(parents=True, exist_ok=True)
     return path
--- tests/test_pathtools.py
+++ tests/test_pathtools.py
@@ -32,3 +32,21 @@ def test_data_path(mocker: MockerFixture, func, folder_to_find):
     assert Path((this_folder_path / 'test_trash_dir') / folder_to_find).exists()
 
 
+@pytest.mark.usefixtures('mocker')
+@pytest.mark.parametrize('func,folder_to_find', [
+    (pth.data_path, 'data'),
+    (pth.tmp_path, 'data/tmp'),
+    (pth.prod_path, 'data/prod'),
+    (pth.config_path, 'config'),
+])
+def test_data_path(mocker: MockerFixture, func, folder_to_find):
+    this_folder_path = Path(os.path.realpath(__file__)).parent
+    # mocker.patch('utils.pathtools.Path.parent',
+    #              new_callable=mocker.PropertyMock(return_value=this_folder_path / 'test_trash_dir'))
+
+    # path = pth.data_path()
+    path = func(base_path=this_folder_path / 'test_trash_dir')
+    assert Path((this_folder_path / 'test_trash_dir') / folder_to_find).exists()
+
+
+
