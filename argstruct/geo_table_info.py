3b5349e04b3a1b39fb70e08e57406e92b3330d13
Loïc Jouans <loic.jouans@arcep.fr>
2021-09-09 17:17:11 +0200

    DOc doc doc doc doc

--- argstruct/geo_table_info.py
+++ argstruct/geo_table_info.py
@@ -1,3 +1,6 @@
+"""
+Structure de description d'une table de géométries
+"""
 from dataclasses import dataclass
 from typing import Optional
 
@@ -5,7 +8,15 @@ from typing import Optional
 @dataclass
 class GeoInfo:
     """
-    Wrapper pour grouper les paramètres de colonnes contenant des geométries
+    Wrapper pour grouper les paramètres de colonnes contenant des geométries.
+    Décris la colonne `<table_path>.<column>` sous l'éventuelle condition `condition`.
+
+    Condition est ajouté à une clause WHERE.
+    ```
+        SELECT <column>
+        FROM <table_path>
+        (WHERE <condition>)
+    ```
     """
     column: str
     table_path: str
